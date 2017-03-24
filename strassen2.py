2b35026d7843bafcca5d27cff778bd23100f86f8
alexnie01 <anie@college.harvard.edu>
2017-03-24 02:58:18 -0400

    finished strassen's with smart padding

--- strassen2.py
+++ strassen2.py
@@ -22,7 +22,7 @@ Taking in inputs
 python strassen.py arg1 arg2 arg3
 """
 # switchover dimension for Strassen to classical matrix multiplication
-cutoff = 31
+cutoff = 4
 
 """
 Generate Matrix
@@ -70,17 +70,41 @@ def std_matmult(mat_1, mat_2, dim):
             prod_mat[i,k] = sum_j 
     return prod_mat
     
-def str_matmult(mat_1, mat_2, dim):
+"""
+Strassen's algorithm and helper functions
+"""
+
+def add_mat(mat_1, mat_2, dim):
+    """ perform matrix addition element-wise """
+    sum_mat = np.zeros((dim,dim), dtype=int)
+    for i in np.arange(dim):
+        for j in np.arange(dim):
+            sum_mat[i,j] = mat_1[i,j]+mat_2[i,j]
+    return sum_mat
+
+def sub_mat(mat_1, mat_2, dim):
+    """ perform matrix subtraction element-wise, mat_1-mat_2 """
+    diff_mat = np.zeros((dim,dim), dtype=int)
+    for i in np.arange(dim):
+        for j in np.arange(dim):
+            diff_mat[i,j] = mat_1[i,j]-mat_2[i,j]
+    return diff_mat
+
+    
+def str_matmult(mat_1, mat_2, dim, cutoff):
+    """ modified Strassen's algorithm. if input dim > switchover cutoff, use
+    standard multiplication. if odd, pad one line of zeros on bottom and right,
+    then recurse on dim+1. if even, use strassen's """
     if dim <= cutoff:
         return std_matmult(mat_1, mat_2, dim)
     else:
         if dim%2==1:
             # pad bottom and right with one line of zeros
-            mat_10 = np.hstack(np.vstack((mat_1, np.zeros((1, dim), dtype=int))), 
-                              np.zeros((1,dim+1), dtype=int))
-            mat_20 = np.hstack(np.vstack((mat_2, np.zeros((1, dim), dtype=int))), 
-                              np.zeros((1,dim+1), dtype=int))
-            return str_matmult(mat_10, mat_20, dim+1)[:-1, :-1]
+            mat_10 = np.hstack((np.vstack((mat_1, np.zeros((1, dim), dtype=int))), 
+                              np.zeros((dim+1,1), dtype=int)))
+            mat_20 = np.hstack((np.vstack((mat_2, np.zeros((1, dim), dtype=int))), 
+                              np.zeros((dim+1,1), dtype=int)))
+            return str_matmult(mat_10, mat_20, dim+1, cutoff)[:-1, :-1]
         else:
             half = dim/2
             # blocks of first matrix
@@ -96,19 +120,22 @@ def str_matmult(mat_1, mat_2, dim):
             b22 = mat_2[half:, half:]
             
             # strassen intermediates
-            p1 = str_matmult(add_mat(a11,a22), add_mat(b11,b22))
-            p2 = str_matmult(add_mat(a21, a22), b11)
-            p3 = str_matmult(a11, add_mat(b12,-b22))
-            p4 = str_matmult(a22, add_mat(b21, -b11))
-            p5 = str_matmult(add_mat(a11, a12), b22)
-            p6 = str_matmult(add_mat(a21, -a11), add_mat(b11, b12))
-            p7 = str_matmult(add_mat(a12, -a22), add_mat(b21, b22))
+            p1 = str_matmult(add_mat(a11,a22, half), add_mat(b11,b22, half), 
+                             half, cutoff)
+            p2 = str_matmult(add_mat(a21, a22, half), b11, half, cutoff)
+            p3 = str_matmult(a11, sub_mat(b12,b22, half), half, cutoff)
+            p4 = str_matmult(a22, sub_mat(b21, b11, half), half, cutoff)
+            p5 = str_matmult(add_mat(a11, a12, half), b22, half, cutoff)
+            p6 = str_matmult(sub_mat(a21, a11, half), add_mat(b11, b12, half),
+                             half, cutoff)
+            p7 = str_matmult(sub_mat(a12, a22, half), add_mat(b21, b22, half),
+                             half, cutoff)
             
             # product subblocks
-            c11 = add_mat(add_mat(add_mat(p1, p4), -p5), p7)
-            c12 = add_mat(p3, p5)
-            c21 = add_mat(p2, p4)
-            c22 = add_mat(add_mat(add_mat(p1, -p2), p3), p6)
+            c11 = add_mat(sub_mat(add_mat(p1, p4, half), p5, half), p7, half)
+            c12 = add_mat(p3, p5, half)
+            c21 = add_mat(p2, p4, half)
+            c22 = add_mat(add_mat(sub_mat(p1, p2, half), p3, half), p6, half)
             
             # combine into product matrix
             return np.vstack((np.hstack((c11,c12)), np.hstack((c21,c22))))
@@ -122,29 +149,7 @@ if __name__ == "__main__":
     mat_2 = data[dim**2:].reshape(dim,dim)
 #    sys.stdout.write(str(mat_1))
 #    sys.stdout.write(str(mat_2))
-    
-    print np.dot(mat_1,mat_2)-std_matmult(mat_1,mat_2,dim)
 
-## #### Strassen Helper Functions
-#
-#def matrix_addition(matrix_1, matrix_2, dimension):
-#    matrix = [[matrix_1[i][j] + matrix_2[i][j] for j in range(dimension)] for i in range(dimension)]
-#    return np.array(matrix).reshape(dimension, dimension)
-#
-#
-#def matrix_subtraction(matrix_1, matrix_2, dimension):
-#    matrix = [[matrix_1[i][j] - matrix_2[i][j] for j in range(dimension)] for i in range(dimension)]
-#    return np.array(matrix).reshape(dimension, dimension)
-
-
-#
-## #### Strassens
-#
-#def multiply_matrices_straussens(matrix_1, matrix_2, dimension):
-#    # first check if small enough to switch to standard
-#    if dimension <= cutoff:
-#        return multiply_matrices_standard(matrix_1, matrix_2, dimension)
-#    
 #
 #    # deal with it if it's not a power of two
 #    is_power_two = True
@@ -157,44 +162,7 @@ if __name__ == "__main__":
 #        matrix_1 = pad_zeros(matrix_1, dimension_difference)
 #        matrix_2 = pad_zeros(matrix_2, dimension_difference)
 #        dimension = closest_power_of_two
-#        
-#    new_dimension = dimension / 2
-#    # matrix 1 blocks
-#    a = matrix_1[0:new_dimension, 0:new_dimension]
-#    b = matrix_1[0:new_dimension, new_dimension:dimension]
-#    c = matrix_1[new_dimension:dimension, 0:new_dimension]
-#    d = matrix_1[new_dimension:dimension, new_dimension:dimension]
-#    # matrix 2 blocks
-#    e = matrix_2[0:new_dimension, 0:new_dimension]
-#    f = matrix_2[0:new_dimension, new_dimension:dimension]
-#    g = matrix_2[new_dimension:dimension, 0:new_dimension]
-#    h = matrix_2[new_dimension:dimension, new_dimension:dimension]
-#    
-#    # calculate intermediaries
-#    P1 = multiply_matrices_straussens(a, matrix_subtraction(f, h, new_dimension), new_dimension)
-#    P2 = multiply_matrices_straussens(matrix_addition(a,b, new_dimension), h, new_dimension)
-#    P3 = multiply_matrices_straussens(matrix_addition(c, d, new_dimension), e, new_dimension)
-#    P4 = multiply_matrices_straussens(d, matrix_subtraction(g, e, new_dimension), new_dimension)
-#    P5 = multiply_matrices_straussens(matrix_addition(a, d, new_dimension), matrix_addition(e, h, new_dimension), 
-#                                      new_dimension)
-#    P6 = multiply_matrices_straussens(matrix_subtraction(b, d, new_dimension), matrix_addition(g, h, new_dimension), 
-#                                      new_dimension)
-#    P7 = multiply_matrices_straussens(matrix_subtraction(a, c, new_dimension), matrix_addition(e, f, new_dimension), 
-#                                      new_dimension)
-#    
-#    # indices refer to blocks of product matrix
-#    ae_bg = matrix_addition(matrix_subtraction(matrix_addition(P5, P4, new_dimension), P2, new_dimension), P6, new_dimension)
-#    af_bh = matrix_addition(P1, P2, new_dimension)
-#    ce_dg = matrix_addition(P3, P4, new_dimension)
-#    cf_dh = matrix_subtraction(matrix_subtraction(
-#            matrix_addition(P5, P1, new_dimension), P3, new_dimension), P7, new_dimension)
-#    final_matrix = np.vstack((np.hstack((ae_bg, af_bh)), np.hstack((ce_dg, cf_dh))))
-#    final_matrix = final_matrix.reshape(dimension, dimension)
-#    
-#    # check to see if we need to remove padding in product matrix
-#    if dimension_difference != 0:
-#        final_matrix = remove_zeros(final_matrix, dimension_difference)
-#    return final_matrix
+
 #
 #
 ## # ### Testing
